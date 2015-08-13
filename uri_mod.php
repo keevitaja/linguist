0bbd6eb8733b4db2a5efd9238cf9823a7c0d6aa4
Tanel Tammik <keevitaja@gmail.com>
2015-08-13 12:10:47 +0300

    fix localized home

--- uri_mod.php
+++ uri_mod.php
@@ -1,19 +1,19 @@
 <?php
 
 /**
- * This hack removes the i18n slug from the REQUEST_URI
+ * This hack removes the locale slug from the REQUEST_URI
  *
  * Totally harmless :)
  */
+
 $config = include 'resources/config/linguist.php';
 
-$pattern = '/^\/('.implode('|', $config['locales']).')\//';
+$pattern = '/^\/('.implode('|', $config['locales']).')(?:\/|$)/';
+
 $uri = filter_input(INPUT_SERVER, 'REQUEST_URI', FILTER_SANITIZE_URL);
 
 if (preg_match($pattern, $uri, $matches)) {
-    $uri = preg_replace($pattern, '/', $uri);
-
-    $_SERVER['REQUEST_URI'] = $uri;
+    $_SERVER['REQUEST_URI'] = preg_replace($pattern, '/', $uri);
 
     define('LOCALE', $matches[1]);
 }
