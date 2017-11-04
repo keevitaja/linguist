d72aae1fe3bc8cf0cc408ee4622d2ca96f84199f
Tanel Tammik <keevitaja@gmail.com>
2017-11-04 22:31:25 +0200

    Revert "remove tests"

---
+++ tests/HtmlBuilderTests.php
@@ -0,0 +1,80 @@
+<?php
+
+class HtmlBuilderTest extends Orchestra\Testbench\TestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->config = app('Illuminate\Config\Repository');
+        $this->url = app('Illuminate\Routing\UrlGenerator');
+
+        $this->config->set('app.locale', 'en');
+        $this->config->set('linguist.default', 'en');
+        $this->config->set('linguist.locales', ['en', 'fr', 'et']);
+
+        $this->url->forceRootUrl('http://keevitaja.com');
+    }
+
+    public function test_link_to_hide_default_false()
+    {
+        $this->config->set('linguist.hide_default', false);
+
+        $attributes = ['class' => 'some-class'];
+        $linkFromBuilder = lnk_to('some/uri', 'Some Title', $attributes);
+        $linkToTestAgainst = '<a class="some-class" href="http://keevitaja.com/en/some/uri">Some Title</a>';
+
+        $this->assertEquals($linkFromBuilder, $linkToTestAgainst);
+    }
+
+    public function test_link_to_hide_default_true()
+    {
+        $this->config->set('linguist.hide_default', true);
+
+        $attributes = ['class' => 'some-class'];
+        $linkFromBuilder = lnk_to('some/uri', 'Some Title', $attributes);
+        $linkToTestAgainst = '<a class="some-class" href="http://keevitaja.com/some/uri">Some Title</a>';
+
+        $this->assertEquals($linkFromBuilder, $linkToTestAgainst);
+    }
+
+    public function test_link_to_route_hide_default_false()
+    {
+        $this->config->set('linguist.hide_default', false);
+
+        app('router')->get('test/this/route', ['as' => 'named.route', function() {}]);
+
+        $attributes = ['class' => 'some-class'];
+        $linkFromBuilder = lnk_to_route('named.route', 'Some Title', [], $attributes);
+        $linkToTestAgainst = '<a class="some-class" href="http://keevitaja.com/en/test/this/route">Some Title</a>';
+
+        $this->assertEquals($linkFromBuilder, $linkToTestAgainst);
+    }
+
+    public function test_link_to_route_hide_default_true()
+    {
+        $this->config->set('linguist.hide_default', true);
+
+        app('router')->get('test/this/route', ['as' => 'named.route', function() {}]);
+
+        $attributes = ['class' => 'some-class'];
+        $linkFromBuilder = lnk_to_route('named.route', 'Some Title', [], $attributes);
+        $linkToTestAgainst = '<a class="some-class" href="http://keevitaja.com/test/this/route">Some Title</a>';
+
+        $this->assertEquals($linkFromBuilder, $linkToTestAgainst);
+    }
+
+    public function test_link_to_route_secure()
+    {
+        $this->config->set('linguist.hide_default', false);
+        $this->url->forceRootUrl('https://keevitaja.com');
+
+        app('router')->get('test/show/{test}', ['as' => 'test.show', function() {}]);
+
+        $attributes = ['class' => 'test'];
+        $linkFromBuilder = lnk_to_route('test.show', 'nimetus', [3], $attributes, [], true);
+        $linkToTestAgainst = '<a class="test" href="https://keevitaja.com/en/test/show/3">nimetus</a>';
+
+        $this->assertEquals($linkFromBuilder, $linkToTestAgainst);
+    }
+}
